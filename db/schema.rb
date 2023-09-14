a0a882214fef4516ec6d1ebf558d141ed7b24405
patrickaaronkim <pkim050@gmail.com>
2023-09-14 16:40:33 -0700

    added an index of username for user table so that it can be unique.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_205033) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_233856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_14_205033) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
