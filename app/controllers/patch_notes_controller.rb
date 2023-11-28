d94f2a457fcde7c082c65e663b416c6ae2aff735
patrickaaronkim <pkim050@gmail.com>
2023-11-28 12:27:11 -0800

    made it so that the footer is similar to navbar in terms of sticky, color theme, and other attributes. got rid of h-screen attribute in homepage as it was bugging out the footer completely. adjusted the button to top funtionality to be above the footer section in all pages. added in placeholder for patch notes RESTful method on both routes and controller. figured out how to output contents inside html file including the tags. next step is to link the patch notes correctly and updating the patch notes to its final product.

--- app/controllers/patch_notes_controller.rb
+++ app/controllers/patch_notes_controller.rb
@@ -2,4 +2,10 @@
 
 class PatchNotesController < ApplicationController
   def index; end
+
+  def boom
+    Rails.logger.info(params)
+    Rails.logger.info('hello')
+    # raw(File.read("./app/views/patch_notes/#{test}"))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   get 'patch_notes/index'
+
+  get 'patch_notes/:name', to: 'patch_notes#boom', constraints: { name: /v[0-9]+.[0-9]+.[0-9]+/ }
+
   resources :blog_posts do
     resources :blog_comments
   end
