eff09b0b48a449e9b494c2c10750360cae7e5233
patrickaaronkim <pkim050@gmail.com>
2024-03-18 17:26:08 -0700

    Initial commit. Updated version from 8.3.0 to 8.4.0. Implemented Resume Upload functionality. Updated deploy commands to fix rubocop error. Added some RSpec testing for upload functionality. Added bullet point and crossed it out in the todo list at the home page. Deleted old pdf file and added new one. Changed the url of resume from navbar on both regular and hamburger menu to dynamically show users the uploaded resume in pdf format.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,44 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 class PagesController < ApplicationController
   def home; end
 
   def about; end
 
   def projects; end
+
+  def upload_resume_page; end
+
+  def upload_resume
+    redirect_to upload_resume_path && return if params[:file_upload].nil?
+
+    tmp = params[:file_upload][:file]
+
+    extension_check(tmp)
+
+    redirect_to upload_resume_path
+  end
+
+  private
+
+  def extension_check(tmp)
+    if tmp.original_filename.split('.').last.include? 'pdf'
+      file = Rails.public_path.join(tmp.original_filename)
+      file_check(tmp.original_filename)
+      FileUtils.cp tmp.tempfile.path, file
+      flash[:notice] = "File #{tmp.original_filename} has successfully uploaded."
+    else
+      flash[:error] = ["File #{tmp.original_filename} is not a pdf file, please try again."]
+    end
+  end
+
+  def file_check(tmp)
+    return unless tmp.include? 'resume'
+
+    Rails.public_path.children.select { |file| file.extname == '.pdf' }.each do |file|
+      Rails.public_path.join(file.basename.to_s).delete
+    end
+  end
 end
